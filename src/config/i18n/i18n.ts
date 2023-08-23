49ca1bb6958031481a4961292dd901f4b868e5a7
IvaanTorres <ivantorresgarcia.02@gmail.com>
2023-08-23 18:41:02 +0200

    done

---
+++ src/config/i18n/i18n.ts
@@ -0,0 +1,16 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import en from "./locale/en";
+import de from "./locale/de";
+import fr from "./locale/fr";
+
+i18n.use(initReactI18next).init({
+ resources: {
+  en: en,
+  de: de,
+  fr: fr
+ }, // Where we're gonna put translations' files
+ lng: "en",     // Set the initial language of the App
+});
+
+export default i18n;
