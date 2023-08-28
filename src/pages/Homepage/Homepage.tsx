1b6c60e581f32a6c873a4a9ee11184dcd29be677
Iván Torres García <ivantorresgarcia.02@gmail.com>
2023-08-28 14:09:12 +0200

    feat(general): change the welcome message size + display it (#42)

--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -2,9 +2,26 @@ import { useTranslation } from "react-i18next"
 import AppLayout from "../../layouts/AppLayout/AppLayout"
 import { homepagePageStyle } from "./style/style"
 import { USER } from "../../shared/constants/localstorage"
+import { useState, useEffect } from "react"
+import { getGeneralPreferences } from "../../shared/services/preferences/general"
 
 const Homepage = () => {
   const { t } = useTranslation()
+  const [welcomeMessageSize, setWelcomeMessageSize] = useState(0)
+
+  useEffect(() => {
+    handleGetMessageSize()
+  }, [])
+
+  const handleGetMessageSize = async () => {
+    const generalPromise = await getGeneralPreferences()
+
+    if('data' in generalPromise){
+      setWelcomeMessageSize(+generalPromise.data.welcomingMessageSize)
+    } else {
+      console.error('Failed to get general preferences');
+    }
+  }
 
   const userEmail = localStorage.getItem(USER) 
     ? JSON.parse(localStorage.getItem(USER) as string).email 
@@ -12,7 +29,7 @@ const Homepage = () => {
 
   return (
     <AppLayout>
-      <div className={homepagePageStyle}>
+      <div className={homepagePageStyle(welcomeMessageSize)}>
         <h1 className="title">{t('homepage.welcome')} {userEmail}</h1>
       </div>
     </AppLayout>
